2bcc1f3aec455f39ef21fac1d50a53c139f65019
WHusssain937 <wasim937@hotmail.com>
2020-06-12 15:20:40 +0000

    added my-books file

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from os import path
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
+if path.exists('env.py'):
+    import env
+
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/my_books')
+def my_books():
+    return render_template("my-books.html", books=mongo.db.books.find())
 
 
 if __name__ == '__main__':
